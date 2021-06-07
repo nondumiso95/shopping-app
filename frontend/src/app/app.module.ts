210e9aedebda3c6b48b5bc408e2d05d7acb94a49
Nattym302 <emanuelklaas2@gmail.com>
2021-06-07 16:36:11 +0200

    login

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LandingComponent } from './landing/landing.component';
+import { NavComponent } from './nav/nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    LandingComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
